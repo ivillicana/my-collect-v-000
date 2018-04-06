31a0cba38840f4842042c6753a0d60d8b6d63ba3
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 19:12:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if array.empty?
+      "Collection is empty!"
+  else
+    i = 0
+    collection = Array.new
+    while i < array.length
+      collection << yield(array[0])
+      i += 1
+    end
+  end  
+end

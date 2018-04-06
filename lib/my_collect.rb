7e4e9e1581d65dac9fe64058044484f50e0d883b
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 19:12:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
     i = 0
     collection = Array.new
     while i < array.length
-      collection << yield(array[0])
+      collection << yield(array[i])
       i += 1
     end
     collection
